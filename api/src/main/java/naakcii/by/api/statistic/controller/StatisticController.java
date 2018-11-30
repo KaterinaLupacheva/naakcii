cc8a0c50448cca55d289defaf3369f7cee96a4a1
cops2002@mail.ru <fuckyouspielberg>
2018-11-30 15:10:10 +0300

    Speedtest

--- api/src/main/java/naakcii/by/api/statistic/controller/StatisticController.java
+++ api/src/main/java/naakcii/by/api/statistic/controller/StatisticController.java
@@ -1,14 +1,11 @@
 package naakcii.by.api.statistic.controller;
 
-import naakcii.by.api.statistic.repository.model.Statistic;
 import naakcii.by.api.statistic.service.StatisticService;
 import naakcii.by.api.statistic.service.modelDTO.StatisticDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 
